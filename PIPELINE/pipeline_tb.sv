a5981001d17be7c84d8c5b3fde0007bda4d85e1b
juanpablo-code2003 <juanpis2140@gmail.com>
2023-11-10 17:11:07 -0500

    Add base for pipeline

---
+++ PIPELINE/pipeline_tb.sv
@@ -0,0 +1,35 @@
+module pipeline_tb;
+  reg clk;
+  reg reset;
+  reg[31:0] initial_address;
+  reg tr;
+
+  pipeline dut (
+    .clk(clk),
+    .reset(reset),
+    .initial_address(initial_address),
+    .tr(tr)
+  );
+
+  always #5 clk = ~clk;
+
+  initial begin
+    $dumpfile("pipeline_tb.vcd");
+    $dumpvars(0, pipeline_tb);
+
+    clk = 0;
+    reset = 1;
+    initial_address = 0;
+
+    #10; 
+    reset = 0;
+
+    #10 tr = 1;
+
+    #6;
+
+    $finish;
+  end
+
+
+endmodule
